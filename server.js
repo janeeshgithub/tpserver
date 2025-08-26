32fdb214ec6dca92d095bd64ab0cbb6bf49cd0cb
JANEESH P <janeeshthelegend@gmail.com>
2025-08-27 01:42:16 +0530

    f

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const PORT = 5000;
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Example route
+app.get('/', (req, res) => {
+    res.send('Hello from Node.js backend!');
+});
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
